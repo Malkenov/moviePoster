13a8b5c28882684b63c2a6f7dff90da094ec9edc
asanali_malkenov22@icloud.com <asanali_1997@mail.ru>
2026-04-07 00:21:08 +0500

    consumer по отмены бронирования

--- src/main/java/moviePoster/com/ComApplication.java
+++ src/main/java/moviePoster/com/ComApplication.java
@@ -2,8 +2,10 @@ package moviePoster.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // аннотация для включение планировщика для задач Spring
 public class ComApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/moviePoster/com/service/consumer/SeatExpiredConsumer.java
@@ -0,0 +1,9 @@
+package moviePoster.com.service.consumer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SeatExpiredConsumer {
+}
